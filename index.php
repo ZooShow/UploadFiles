e5df27996714e09f48f632537a5e31a144c87f84
Fedor Zhuravlev <zhuravlev_fa@drom.ru>
2022-05-17 18:05:42 +1000

    Fix

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 
 $projectDir = dirname(__FILE__) . '/src/';
 $uploadfile = $projectDir . basename($_FILES['userfile']['name']);
+
+if (!is_dir($projectDir)) {
+    mkdir($projectDir);
+}
+
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "Файл был успешно загружен." . PHP_EOL;
 } else {
     echo 'Код ошибки: ' . $_FILES['userfile']['error'] . PHP_EOL;
 }
-print "</pre>";
